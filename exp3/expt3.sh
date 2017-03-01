0c7de7024ca18ab2b96b69e3d170ba090a1ae87d
ctse-16 <ctse-16@ctse16.(none)>
2017-03-01 12:04:31 +0530

    exp3

---
+++ exp3/expt3.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#Currently logged user and his logname
+# Your current shell
+#Your home directory
+# Your operating system type
+# Your current path setting
+# Your current working directory
+# Show Currently logged number of users
+
+
+echo "User name : $USER(login name:$LOGNAME)"
+echo "Cureent Shell : $SHELL"
+echo "path : $PATH"
+echo "PRESENT WORKING DIRECTORY : $PWD"
+echo "your OS type:$OSTYPE"
+echo "home directory : $HOME"
+echo "currently logged:$nouser user(s)"
+
+
+
+
+
