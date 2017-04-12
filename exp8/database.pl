d2837cd2e26d27addb0de5f83a155f140105e33f
ctse-16 <ctse-16@ctse16.(none)>
2017-04-12 11:19:05 +0530

    last ecpt only remains

--- exp8/database.pl
+++ exp8/database.pl
@@ -6,12 +6,12 @@ my $driver = "mysql";
 my $database = "student";
 my $dsn = "DBI:$driver:database=$database";
 my $userid = "root";
-my $password = "";
+my $password = "password";
 my $dbh = DBI->connect($dsn, $userid, $password ) or die $DBI::errstr;
 my $sth = $dbh->prepare("INSERT INTO student
                        (name , phone )
                         values
-                       ('poke',111);");
+                       ('saharsh',11145);");
 $sth->execute() or die $DBI::errstr;
 $sth->finish();
 #$dbh->commit or die $DBI::errstr;
